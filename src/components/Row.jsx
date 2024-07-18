fbd7da28f25eb9c78e845a402b9c358d6aaed9c0
yamilzavala <zavalayamil9@gmail.com>
2024-07-18 18:33:57 -0300

    added show all

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -9,77 +9,78 @@ export default function Row({
   children,
   collapse,
   id,
-  rowPosition
 }) {
   const [expanded, setExpanded] = useState(false);
-  const rendrerChildren = children?.map((child, idx) => {
-    const idIncreased = idx+1;
+
+  const renderedChildren = children?.map((child, idx) => {    
     const childProps = {
       ...child,
-      id: idIncreased
+      id: idx + 1,
     }
-    return (<div id={idIncreased} className={expanded ? "expanded" : "collapsed"}>
-      <Row key={idx} {...childProps } />
-    </div>)
+    return (     
+        <Row key={idx} {...childProps } />
+    )
   });
 
   const handleShowChildren = () => {
     setExpanded(!expanded);
   };
 
-  const handleCollapse = () => {
-    //REFACTOR
-    if(id === 0) {
-      setExpanded(true);
-    } else {
-      children?.forEach(child => {
-        if (collapse) {
-          setExpanded(false);
-        } else {
-          setExpanded(true);
-        }
-        // child.setExpanded(true);
-        // console.log(child)
-      });
-      // if (collapse) {
-      //   setExpanded(false);
-      // } else {
-      //   setExpanded(true);
-      // }
-    }
-  }
 
+  //effect to collapse or expand children cotainer
   useEffect(() => {   
-    handleCollapse();
+    setExpanded(!expanded);
+    const childrenList = document.querySelectorAll('.expanded, .collapsed');
+
+    childrenList.forEach(item => {
+      if(Number(item.id) === 0) {
+         item.classList.remove('collapsed')
+         item.classList.add('expanded')
+      } else {
+        if(expanded) {
+          item.classList.remove('collapsed')
+          item.classList.add('expanded')
+        } else {
+          item.classList.remove('expanded')
+          item.classList.add('collapsed')
+        }
+      }
+    })
   }, [collapse]);
 
   return (
     <>
-      <div id={id}
-        className={level < 4 ? "bg-row-level-1-to-3 row" : "bg-row-level-4 row"}
-      >
-        {/* cell 1 */}
-        <Cell
-          value={wbsElement}
-          position={0}
-          level={level}
-          handleShowChildren={handleShowChildren}
-          expanded={expanded}
-          hasChildren={children?.length > 0}
-          id={id}
-        />
+      {/* row */}
+      <div id={id} className={level < 4 ? "bg-row-level-1-to-3 row" : "bg-row-level-4 row"}>  
+          {/* cell 1 */}
+          <Cell
+            value={wbsElement}
+            position={0}
+            level={level}
+            handleShowChildren={handleShowChildren}
+            expanded={id === 0 ? true : expanded}
+            hasChildren={children?.length > 0}
+            id={id}
+          />
 
-        {/* cell 2 */}
-        <Cell value={levelDescription} position={1} expanded={expanded} id={id} level={level}/>
+          {/* cell 2 */}
+          <Cell value={levelDescription} position={1} expanded={id === 0 ? true : expanded} id={id} level={level}/>
 
-        {/* cell 3 */}
-        <Cell value={description} position={2} expanded={expanded} id={id} level={level}/>
+          {/* cell 3 */}
+          <Cell value={description} position={2} expanded={id === 0 ? true : expanded} id={id} level={level}/>
 
-        {/* cell 4 */}
-        <Cell value={legalDescription} position={3} expanded={expanded} id={id} level={level}/>
-      </div>
+          {/* cell 4 */}
+          <Cell value={legalDescription} position={3} expanded={id === 0 ? true : expanded} id={id} level={level}/>
+      </div>     
+
       {/* children */}      
-        {rendrerChildren}
+      {children?.length ? 
+        (          
+          <div id={id} class='expanded'>            
+            {renderedChildren}        
+          </div>
+        ) : null}     
+      
     </>
   );
 }
